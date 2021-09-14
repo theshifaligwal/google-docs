235ae3116b2608458dfc2d838d53e410e0cc9794
theshifaligwal <gwal.shifali2223@gmail.com>
2021-09-14 12:41:42 +0530

    Section page complete

--- pages/index.js
+++ pages/index.js
@@ -33,8 +33,11 @@ export default function Home() {
 
 
           <div>
+          <div className="relative h-52 w-40 border cursor-pointer hover:border-blue-700">
           <Image src='https://links.papareact.com/pju' layout="fill" />
+          </div>
           
+          <p className="ml-2 mt-2 font-semibold text-sm text-gray-700">Blank</p>
           </div>
         </div>
       </section>
