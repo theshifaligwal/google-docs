e5a90929cd4d7bc7118483ca09cfdbeb7acb5714
theshifaligwal <gwal.shifali2223@gmail.com>
2021-09-14 12:52:02 +0530

    Create document section

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,8 @@
 import Head from "next/head";
 import Header from "../components/Header";
 import Button from "@material-tailwind/react/Button";
-import Icon from "@material-tailwind/react/Icon"
-import Image from "next/image"
-
+import Icon from "@material-tailwind/react/Icon";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -21,23 +20,34 @@ export default function Home() {
             <h2 className="text-gray-700 text-lg">Start a new document</h2>
 
             <Button
-            color="gray"
-            buttonType="outline"
-            iconOnly={true}
-            ripple="dark"
-            className="border-0"
-          >
-            <Icon name="more_vert" size="4xl" />
-          </Button>
+              color="gray"
+              buttonType="outline"
+              iconOnly={true}
+              ripple="dark"
+              className="border-0"
+            >
+              <Icon name="more_vert" size="4xl" />
+            </Button>
           </div>
-
 
           <div>
-          <div className="relative h-52 w-40 border cursor-pointer hover:border-blue-700">
-          <Image src='https://links.papareact.com/pju' layout="fill" />
+            <div className="relative h-52 w-40 border cursor-pointer hover:border-blue-700">
+              <Image src="https://links.papareact.com/pju" layout="fill" />
+            </div>
+
+            <p className="ml-2 mt-2 font-semibold text-sm text-gray-700">
+              Blank
+            </p>
           </div>
-          
-          <p className="ml-2 mt-2 font-semibold text-sm text-gray-700">Blank</p>
+        </div>
+      </section>
+
+      <section className="bg-white px-10 md:px-0">
+        <div className="max-w-3xl mx-auto py-8 text-sm text-gray-700">
+          <div className="flex items-center justify-between pb-5">
+            <h2 className="font-medium flex-fgrow">My Document</h2>
+            <p className="mr-12">Data Created</p>
+            <Icon name="folder" size="3xl" color="gray" />
           </div>
         </div>
       </section>
