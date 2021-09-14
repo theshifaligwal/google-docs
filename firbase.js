4ef027f47086da64c4fee27ebdb339de0c9f4d69
theshifaligwal <gwal.shifali2223@gmail.com>
2021-09-14 13:28:20 +0530

    Create firebase file

---
+++ firbase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+const firebaseConfig = {
+  apiKey: "AIzaSyB7b7m-wlPBgI3Hory10wMOMR-_WeBJO3M",
+  authDomain: "docs-40b80.firebaseapp.com",
+  projectId: "docs-40b80",
+  storageBucket: "docs-40b80.appspot.com",
+  messagingSenderId: "328644162338",
+  appId: "1:328644162338:web:30b816b5adcae8b95d2b7b",
+};
+
+const app = !firebase.apps.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app();
+
+const db = app.firestore();
+
+export { db };
