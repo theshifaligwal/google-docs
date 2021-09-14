c0f965f9d1d00fca4c6a66219d2e921986f0bfc2
theshifaligwal <gwal.shifali2223@gmail.com>
2021-09-14 10:23:17 +0530

    Header implementation done

--- components/Header.js
+++ components/Header.js
@@ -1,19 +1,37 @@
-import Button from "@material-tailwind/react/Button"
-import Icon from "@material-tailwind/react/Icon"
+import Button from "@material-tailwind/react/Button";
+import Icon from "@material-tailwind/react/Icon";
 export default function Header() {
-    return (
-        <div className="sticky z-50 top-0 flex items-center px-4 py-2 shadow-md bg-white">
-            <Button color="blue"
-            buttonType="outline"
-            rounded={true}
-            iconOnly={true}
-            ripple="dark"
-            className="h-20 w-20 border-0"
-            
-            > 
-            <Icon name="menu" size="3xl" /> </Button>
-            <Icon name="description" size="5xl" color="blue" />
-            <h1 className="ml-2 text-gray-700 text-2xl">Docs</h1>
-        </div>
-    )
+  return (
+    <div className="sticky z-50 top-0 flex items-center px-4 py-2 shadow-md bg-white">
+      <Button
+        color="gray"
+        buttonType="outline"
+        rounded={true}
+        iconOnly={true}
+        ripple="dark"
+        className="h-20 w-20 border-0"
+      >
+        <Icon name="menu" size="3xl" />{" "}
+      </Button>
+      <Icon name="description" size="5xl" color="blue" />
+      <h1 className="ml-2 text-gray-700 text-2xl">Docs</h1>
+
+      <div className="flex flex-grow items-center md:mx-20 mx-4 px-5 py-2 bg-gray-100 text-gray-600 rounded-lg focus-within:text-gray-800 focus-within:shadow-md">
+        <Icon name="search" size="3xl" color="gray" />
+        <input type="text" placeholder="Search" className="flex-grow px-5 text-base bg-transparent outline-none" />
+      </div>
+      <Button
+        color="gray"
+        buttonType="outline"
+        rounded={true}
+        iconOnly={true}
+        ripple="dark"
+        className="ml-5 md:ml-20 h-20 w-20 border-0"
+      >
+        <Icon name="apps" size="4xl" color="fray" />
+      </Button>
+
+      <img loading="lazy" className="cursor-pointer h-12 w-12 rounded-full ml-2" src="https://lh3.googleusercontent.com/ogw/ADea4I6g1VuppzZsS5EZC7wCCWeX1v_V3qXiZAPz0Ng=s32-c-mo" alt=""/>
+    </div>
+  );
 }
