4881e27d106e669f77ac77eb77466849ee2a32a8
theshifaligwal <gwal.shifali2223@gmail.com>
2021-09-14 09:34:40 +0530

    Create header component

---
+++ components/Header.js
@@ -0,0 +1,9 @@
+import Button from "@material-tailwind/react/Button"
+import Icon from "@material-tailwind/react/Icon"
+export default function Header() {
+    return (
+        <div className="flex items-center">
+            
+        </div>
+    )
+}
