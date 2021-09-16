654c6ac50ea1a3c7c3a16f6f171eb089a8abd616
theshifaligwal <gwal.shifali2223@gmail.com>
2021-09-16 13:37:14 +0530

    Create modal

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,49 @@ import Icon from "@material-tailwind/react/Icon";
 import Image from "next/image";
 import { getSession, useSession } from "next-auth/client";
 import Login from "../components/Login";
+import { useState } from "react";
+import Modal from "@material-tailwind/react/Modal";
+import ModalBody from "@material-tailwind/react/ModalBody";
+import ModalFooter from "@material-tailwind/react/ModalFooter";
 
 export default function Home() {
   const [session] = useSession();
+  const [showModal, setShowModal] = useState(false);
+  const [input, setInput] = useState("");
 
   if (!session) return <Login />;
+
+  const createDocument = () => {}
+
+  const modal = (
+    <Modal size="sm" active={showModal} toggler={() => setShowModal(false)}>
+      <ModalBody>
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          type="text"
+          className="outline-none w-full"
+          placeholder="Enter name of document..."
+          onKeyDown={(e) => e.key === "Enter" && createDocument()}
+        />
+      </ModalBody>
+
+      <ModalFooter>
+        <Button
+          color="blue"
+          buttonType="link"
+          onClick={(e) => setShowModal(false)}
+          ripple="dark"
+        >
+          Cancel
+        </Button>
+
+        <Button color="blue" onClick={createDocument} ripple="light">
+          Create
+        </Button>
+      </ModalFooter>
+    </Modal>
+  );
 
   return (
     <div className="">
@@ -19,6 +57,7 @@ export default function Home() {
       </Head>
 
       <Header />
+      {modal}
 
       <section className="bg-[#F8F9FA] pb-10 px-10">
         <div className="max-w-3xl mx-auto">
@@ -61,14 +100,12 @@ export default function Home() {
   );
 }
 
-
-export async function getServerSideProps(context){
+export async function getServerSideProps(context) {
   const session = await getSession(context);
 
-
-  return{
-    props:{
+  return {
+    props: {
       session,
-    }
-  }
+    },
+  };
 }
