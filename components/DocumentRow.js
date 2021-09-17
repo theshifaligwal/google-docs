81c8f24b3b5b903bf93a9ea16d701b7cb4f49707
theshifaligwal <gwal.shifali2223@gmail.com>
2021-09-17 07:09:58 +0530

    Bugs fix

--- components/DocumentRow.js
+++ components/DocumentRow.js
@@ -2,13 +2,13 @@ import Button from "@material-tailwind/react/Button";
 import Icon from "@material-tailwind/react/Icon";
 import { useRouter } from "next/dist/client/router";
 
-export default function DocumentRow({ id, fileName, date }) {
+export default function DocumentRow({ id, fileName, time }) {
   const router = useRouter();
   return (
     <div onClick={() => router.push(`/doc/${id}`)} className="flex items-center p-4 rounded-lg hover:bg-gray-100 text-gray-700  text-sm cursor-pointer">
       <Icon name="article" size="3xl" color="blue" />
       <p className="flex-grow pl-5 pr-10 truncate">{fileName}</p>
-      <p className="pr-5 text-sm">{date?.toDate().toLocalDateString()}</p>
+      <p className="pr-5 text-sm">{time?.toLocalDateString()}</p>
       <Button
         color="gray"
         buttonType="outline"
--- firebase.js
+++ firebase.js
@@ -3,14 +3,13 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey: "AIzaSyB7b7m-wlPBgI3Hory10wMOMR-_WeBJO3M",
+  authDomain: "docs-40b80.firebaseapp.com",
+  projectId: "docs-40b80",
+  storageBucket: "docs-40b80.appspot.com",
+  messagingSenderId: "328644162338",
+  appId: "1:328644162338:web:30b816b5adcae8b95d2b7b",
 };
-
 
 const app = !firebase.apps.length
   ? firebase.initializeApp(firebaseConfig)
